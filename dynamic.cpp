d8c9e70400ded1c839919763b7d90b17d6de590d
Levi <Daz>
2024-08-29 22:22:11 -0600

    Burronacci

--- dynamic.cpp
+++ dynamic.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <climits>  // Para usar INT_MAX
+#include <climits>
 
 using namespace std;
 
+int Burronacci(int n){
+    if(n < 2){
+        return n;
+    }
+
+    return 4 * Burronacci(n - 1) + 2 * Burronacci(n - 2);
+    
+}
+
 
 int main(){
+
+    int result = Burronacci(2);
+
+    cout << result << endl;
 
     return 0;
 }
