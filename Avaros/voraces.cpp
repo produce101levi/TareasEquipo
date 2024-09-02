d3d55674e23d07db7ea321e277e2eb69229e7241
Levi <Daz>
2024-09-01 19:00:00 -0600

    Avaros

---
+++ Avaros/voraces.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+// Función para determinar la cantidad de cada moneda a regresar
+vector<int> coinChange(int n, vector<int> coins, int p, int q){
+    vector<int> result;
+    int change = abs(p - q);
+
+    sort(coins.begin(), coins.end(), greater<int>());
+
+    // Ciclo para restar del cambio el valor de la moneda
+    // y además aumentar el valor del contador
+    for (int i = 0; i < n; i++){
+        int counter = 0;
+        while(change >= coins[i]){
+            change -= coins[i];
+            counter++;
+        }
+        result.push_back(counter);
+    }
+
+    return result;
+}
+
+int main(){
+
+    int n; // Número de monedas disponibles
+    vector<int> coins; // Valores de cada moneda
+    int p; // Precio del producto
+    int q; // Billete utilizado para pagar
+
+    cout << "Inserta número de monedas disponibles N: ";
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int value;
+        cout << "Inserta el valor de la moneda #" << i + 1 << endl;
+        cin >> value;
+        coins.push_back(value);
+    }
+    cout << "Inserta el precio del producto: ";
+    cin >> p;
+    cout << "Inserta el valor del billete utilizado para pagar el producto: ";
+    cin >> q;
+
+    vector<int> result = coinChange(n, coins, p, q);
+
+    for (int c : result){
+        cout << c << endl;
+    }
+
+    return 0;
+}
