51e0e0bf8928fedb794752a8d60ffae342eeebec
Levi <Daz>
2024-09-29 15:11:44 -0600

    encontrar patrón en texto

--- Situación Problema/situacionproblema.cpp
+++ Situación Problema/situacionproblema.cpp
@@ -3,12 +3,80 @@
 
 using namespace std;
 
-void readFiles(){
+void getZarr(string str, int Z[]);
+
+
+bool search(string text, string pattern)
+{
+
+    string concat = pattern + "$" + text;
+    int l = concat.length();
+
+    int Z[l];
+    getZarr(concat, Z);
+
+    // Print Z array
+    // for (int i = 0; i < l; i++){
+    //     if (i != l - 1){
+    //         cout << Z[i] << ", ";
+    //     } else {
+    //         cout << Z[i] << endl;
+    //     }
+    // }
+
+    for (int i = 0; i < l; ++i)
+    {
+  
+        if (Z[i] == pattern.length()){
+            printf("Patrón %s encontrado en índice Z[%zu]\n", pattern.c_str(), i - pattern.length() - 1);
+            return true;
+        }
+    }
+    return false;
+}
+
+// Fills Z array for given string str[]
+void getZarr(string str, int Z[])
+{
+    int n = str.length();
+    int k, R, L;
+
+    L = R = 0;
+    for (int i = 1; i < n; i++){
+        if(i > R){
+            L = R = i;
+
+            // Extend the Z window
+            while(R < n && str[R-L] == str[R]){
+                R++;
+            }
+            Z[i] = R-L;
+            R--;
+        }else{
+            k = i - L;
+            if(Z[k] < R - i + 1){
+                Z[i] = Z[k];
+            }else{
+                L = i;
+                while(R < n && str[R-L] == str[R]){
+                    R++;
+                }
+                Z[i] = R-L;
+                R--;
+            }
+        }
+    }
+}
+
+
+int main(){
+    
     string myText1;
     string myText2;
-    string myText3;
-    string myText4;
-    string myText5;
+    string pattern1;
+    string pattern2;
+    string pattern3;
+    string line;
 
     ifstream myReadFile1("transmission01.txt");
     ifstream myReadFile2("transmission02.txt");
@@ -17,29 +85,32 @@ void readFiles(){
     ifstream mCode2("mcode02.txt");
     ifstream mCode3("mcode03.txt");
 
-    cout << "-=- TEXT 1 -=-" << endl;
+    vector<string> trans1;
+    vector<string> trans2;
+
+    // Transimission 01
     while(getline(myReadFile1, myText1)){
-        cout << myText1 << endl;
+        trans1.push_back(myText1);
     }
 
-    cout << "-=- TEXT 2 -=-" << endl;
+    // Transimission 02
     while(getline(myReadFile2, myText2)){
-        cout << myText2 << endl;
+        trans2.push_back(myText2);
     }
 
     cout << "-=- MALICIOUS PATTERN -=-" << endl;
-    while(getline(mCode1, myText3)){
-        cout << myText3 << endl;
+    while(getline(mCode1, line)){
+        pattern1 += line;
     }
 
     cout << "-=- MALICIOUS PATTERN -=-" << endl;
-    while(getline(mCode2, myText4)){
-        cout << myText4 << endl;
+    while(getline(mCode2, line)){
+        pattern2 += line;
     }
 
-    cout << "-=- MALICIOUS PATTERN -=-" << endl;
-    while(getline(mCode3, myText5)){
-        cout << myText5 << endl;
+    // Malicious Pattern 3
+    while(getline(mCode3, line)){
+        pattern3 += line;
     }
 
     myReadFile1.close();
@@ -48,11 +119,19 @@ void readFiles(){
     mCode2.close();
     mCode3.close();
 
-}
 
-int main(){
-    
-    readFiles();
-    
+    cout << "Transmission 01:" << endl;
+    for(int i = 0; i < trans1.size(); i++){
+        cout << "LINE " << i + 1 << endl;
+        bool patternFound = search(trans1[i], pattern3);
+        if (patternFound == 1){
+            cout << "True" << endl;
+        } else {
+            cout << "False" << endl;
+        }
+    }
+
+    // search("abclevi", "levi");
+
     return 0;
 }
